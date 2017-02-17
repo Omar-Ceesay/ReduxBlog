7b847c972a69a07c10f54a89972aa70a2ea0d45e
Omar Ceesay <Omar.Ceesay@bluecrossmn.com>
2017-02-17 10:46:40 -0600

    83

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+//import {bindActionCreators} from 'redux';
 
-export default () => {
-  return(
-    <div>
-      List of blog posts
-    </div>
-  )
+import {fetchPosts} from '../actions/index';
+
+class PostsIndex extends Component{
+  componentWillMount(){
+    this.props.fetchPosts();
+  }
+  render(){
+    return(
+      <div>
+        What's up
+      </div>
+    )
+  }
 }
+
+// function mapDispatchToProps(dispatch){
+//   return bindActionCreators({fetchPosts}, dispatch);
+// }
+
+export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
+//connect(passToState, passToProps)
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 import Postindex from './components/post_index';
+import PostsNew from './components/posts_new';
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={Postindex}/>
+    <Route path='/posts/new' component={PostsNew}/>
   </Route>
 );
